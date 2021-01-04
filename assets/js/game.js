0d3a8040c8d3cc82059043a8a3baef1a54a94bd6
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-01-04 01:01:32 -0500

    js start

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+
+var playerName = window.prompt("What is your robot's name?");
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  var name = "name";
+console.log(name);
+  console.log(playerName);
+console.log("Player's name is " + playerName);
+
+ // fight();
